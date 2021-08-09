3b038d0ba2761c15f2d4ef70158b3270d55e99f9
Christopher Løkken <chrlokk1@gmail.com>
2021-08-10 00:24:07 +0200

    docker works with webpack!

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -6,7 +6,8 @@ module.exports = {
         filename: 'index.bundle.js'
     },
     devServer: {
-        port: 3001,
+        host: '0.0.0.0',
+        port: 3000,
         watchContentBase: true
     },
     module: {
