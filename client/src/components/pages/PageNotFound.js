fd3c5cdc65249d7f2547b80ecff0f047dec29908
Christopher Løkken <chrlokk1@gmail.com>
2021-08-15 16:42:45 +0200

    default 404 and under construction pages

--- client/src/components/pages/PageNotFound.js
+++ client/src/components/pages/PageNotFound.js
@@ -2,6 +2,9 @@ import React from "react"
 
 export const PageNotFound = () => {
     return (
-        <h1>Fant ikke siden!</h1>
+        <>
+            <h1>Fant ikke siden!&#x1F62D;</h1>
+            <h3>Klikk <a href="/">her</a> for å gå til hjemsiden</h3>
+        </>
     )
 }
--- client/src/components/pages/TempPage.js
+++ client/src/components/pages/TempPage.js
@@ -3,7 +3,7 @@ import React from "react"
 export const TempPage = () => {
     return (
         <>
-            <h1 style={{textAlign: "center"}}>Denne siden er under oppbygning</h1>
+            <h1 style={{textAlign: "center"}}>Denne siden er under bygging &#128679;</h1>
             <h3>Sjekk innom senere!</h3>
         </>
     )
